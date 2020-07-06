c697fa0c1ba6ad6266d7aa038446df5de9ab79fb
vdonoladev <victordonolaf@gmail.com>
2020-07-06 08:24:39 -0300

    Código Completo (Será atualizado)

---
+++ jogo-jokenpo.py
@@ -0,0 +1,59 @@
+from random import randint
+from time import sleep
+
+lista = ("Pedra", "Papel", "Tesoura")
+
+computador = randint(0,2)
+
+perguntar = int(input('''Escolha uma opção para jogar: 
+
+[0] Pedra
+[1] Papel
+[2] Tesoura
+
+Digite sua escolha: '''))
+
+print("JO\n")
+sleep(1)
+print("KEN\n")
+sleep(1)
+print("POO!\n")
+
+print("-="*20)
+print("O computador escolheu: {}".format(lista[computador]))
+print("O jogador escolheu: {}".format(lista[perguntar]))
+print("-="*20)
+
+if computador == 0:
+	if perguntar == 0:
+		print("Empate!")
+	elif perguntar == 1:
+		print("Jogador perdeu!")
+	elif perguntar == 2:
+		print("Computador venceu!")
+	else:
+		print("Operação Inválida!")
+
+elif computador == 1:
+	if perguntar == 0:
+		print("Computador perdeu!")
+	elif perguntar == 1:
+		print("Empate!")
+	elif perguntar == 2:
+		print("Jogador venceu!")
+	else:
+		print("Operação Inválida!")
+
+elif computador == 2:
+	if perguntar == 0:
+		print("Jogador venceu!")
+	elif perguntar == 1:
+		print("Computador venceu!")
+	elif perguntar == 2:
+		print("Empate!")
+	else:
+		print("Operação Inválida!")
+else:
+	print("Operação Inválida!")
+
+
