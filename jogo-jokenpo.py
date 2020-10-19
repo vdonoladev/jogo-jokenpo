d796a43a633101f6e96c2414c34c81953dbe0714
vdonoladev <victordonolaf@gmail.com>
2020-10-19 08:14:56 -0300

    Updates

--- jogo-jokenpo.py
+++ jogo-jokenpo.py
@@ -3,7 +3,7 @@ from time import sleep
 
 lista = ("Rock", "Paper", "Scissors")
 
-computer = randint(0,2)
+computer = randint(0, 2)
 
 ask = int(input('''Choose an option to play:
 
@@ -25,33 +25,33 @@ print("The player chose: {}".format(lista[ask]))
 print("-="*20)
 
 if computer == 0:
-	if ask == 0:
-		print("A tie!")
-	elif ask == 1:
-		print("Player lost!")
-	elif ask == 2:
-		print("Computer won!")
-	else:
-		print("Invalid Operation!")
+    if ask == 0:
+        print("A tie!")
+    elif ask == 1:
+        print("Player lost!")
+    elif ask == 2:
+        print("Computer won!")
+    else:
+        print("Invalid Operation!")
 
 elif computer == 1:
-	if ask == 0:
-		print("Computer lost!")
-	elif ask == 1:
-		print("A tie!")
-	elif ask == 2:
-		print("Player won!")
-	else:
-		print("Invalid Operation!")
+    if ask == 0:
+        print("Computer lost!")
+    elif ask == 1:
+        print("A tie!")
+    elif ask == 2:
+        print("Player won!")
+    else:
+        print("Invalid Operation!")
 
 elif computer == 2:
-	if ask == 0:
-		print("Player won!")
-	elif ask == 1:
-		print("Computer won!")
-	elif ask == 2:
-		print("A tie!")
-	else:
-		print("Invalid Operation!")
+    if ask == 0:
+        print("Player won!")
+    elif ask == 1:
+        print("Computer won!")
+    elif ask == 2:
+        print("A tie!")
+    else:
+        print("Invalid Operation!")
 else:
-	print("Invalid Operation!")
+    print("Invalid Operation!")
